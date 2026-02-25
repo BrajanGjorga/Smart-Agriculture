721962cdfec770cdf6b3a356c8895e82418bbd39
BrajanGjorga <brajangjorga@2rmlab.net>
2026-02-25 02:41:46 +0100

    This is the first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+import requests
+
+
+api_key = "KGAT_1afb81b502f85ae1e01f5e6db37dbd2b"
+
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("wisam1985/iot-agriculture-2024")
+
+print("Path to dataset files:", path)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, welcome to the Smart Agriculture API!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
